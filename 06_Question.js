210db3e11b728d5004afca1cc254ff1400db17db
AbedinAkif <akifabedin876@gmail.com>
2021-08-05 01:52:03 -0700

    JavaSript Assignment
    Assignment on Javascript

---
+++ 06_Question.js
@@ -0,0 +1,5 @@
+const set = new Set(['Beethoven', 'Mozart', 'Chopin', 'Chopin'])
+
+var c = set.delete('Beethoven');
+// console.log(c);
+console.log(set);
---
+++ 10_Question.js
@@ -0,0 +1,14 @@
+// function Hero(name, level)
+// {
+// name = name;
+// level = level;
+// }
+
+// above code is just a function
+class Hero {
+    constructor(name,level){
+        this.name = name ;
+        this.level = level ;
+    }
+}
+// Therefore, below example is not correct
